e42cfa795668f7591c9b3381dbf93c55e8dba23a
Dina Ibragimova <ibragimova@technokratos.com>
2022-10-10 21:34:00 +0300

    task 2

--- main.py
+++ main.py
@@ -1,47 +1,131 @@
-import pandas as pd
-import matplotlib.pyplot as plt
+import copy
 import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.datasets import make_blobs
+def random_points(n):
+    points = []
+    points = make_blobs(n)
+    return points[0]
+def dist(pntA, pntB):
+    return np.sqrt((pntA[0] - pntB[0]) ** 2 + (pntA[1] - pntB[1]) ** 2)
+def calculate_mean(points):
+    x_mean = 0
+    y_mean = 0
+    count = 0
 
-def read_csv(filename):
-    data = pd.read_csv(filename)
-    return data
+    for i in range(len(points)):
+        x_mean += points[i][0]
+        y_mean += points[i][1]
+        count += 1
 
+    x_mean /= count
+    y_mean /= count
 
-def first_task(data):
-    first_pclass_survived_male = data[(data['Sex'] == "male") & (data['Survived'] == 1) & (data['Pclass'] == 1)]
-    second_pclass_survived_male = data[(data['Sex'] == "male") & (data['Survived'] == 1) & (data['Pclass'] == 2)]
-    third_pclass_survived_male = data[(data['Sex'] == "male") & (data['Survived'] == 1) & (data['Pclass'] == 3)]
+    return [x_mean, y_mean]
+def init_center(points, k):
+    pnt_mean = calculate_mean(points)
+    R = -1
 
-    first_pclass_survived_female = data[(data['Sex'] == "female") & (data['Survived'] == 1) & (data['Pclass'] == 1)]
-    second_pclass_survived_female = data[(data['Sex'] == "female") & (data['Survived'] == 1) & (data['Pclass'] == 2)]
-    third_pclass_survived_female = data[(data['Sex'] == "female") & (data['Survived'] == 1) & (data['Pclass'] == 3)]
+    for i in range(len(points)):
+        current_dist = dist(points[i], pnt_mean);
+        if R < current_dist:
+            R = current_dist
+    centroids = []
+    for i in range(k):
+        x_c = R * np.cos(2 * np.pi * i / k) + pnt_mean[0]
+        y_c = R * np.sin(2 * np.pi * i / k) + pnt_mean[1]
+        centroids.append([x_c, y_c])
 
-    female = np.array([len(first_pclass_survived_female), len(second_pclass_survived_female), len(third_pclass_survived_female)])
-    male = np.array([len(first_pclass_survived_male), len(second_pclass_survived_male), len(third_pclass_survived_male)])
-    width = 0.3
-    x = np.arange(3)
-    fig, ax = plt.subplots()
-    ax.bar(x - width / 2, female, width, label='female')
-    ax.bar(x + width / 2, male, width, label='male')
-    ax.set_xticks(x)
-    ax.set_xticklabels([1, 2, 3])
-    ax.legend()
+    return np.array(centroids)
+def calculate_nearest_centroid(points, centroids):
+    clusters = {}
 
-def second_task(data):
-    pclasses = [len(data[data['Pclass'] == 1]), len(data[data['Pclass'] == 2]), len(data[data['Pclass'] == 3])]
-    fig1, ax1 = plt.subplots()
-    ax1.pie(pclasses, labels=[1, 2, 3], autopct='%1.1f%%')
-    plt.show()
+    for i in range(len(centroids)):
+        clusters[i] = []
 
-def third_task(data):
-    data['Age'].hist(color="green", label="passengers")
-    plt.xlabel('Age')
-    plt.ylabel('Frequency')
-    plt.show()
+    for i in range(len(points)):
+        points_range = []
+
+        for j in range(len(centroids)):
+            points_range.append(dist(points[i], centroids[j]))
+
+        index = points_range.index(min(points_range))
+        clusters[index].append(points[i])
+
+    return clusters
+def centroids_update(centroids, clusters):
+    k = len(centroids)
+    for i in range(k):
+        if (len(clusters[i]) != 0):
+            centroids[i] = calculate_mean(clusters[i])
+    return centroids
+def is_equals(centroids, privious_centroids):
+    return (centroids == privious_centroids).all()
+def draw(clusters, centroids):
+  for i in range(len(clusters)):
+    x_coordinates = []
+    y_coordinates = []
+    for j in range(len(clusters[i])):
+      x_coordinates.append(clusters[i][j][0])
+      y_coordinates.append(clusters[i][j][1])
+    plt.scatter(x_coordinates, y_coordinates)
+  plt.draw()
+
+  x_coordinates_c = []
+  y_coordinates_c = []
+  for i in range(len(centroids)):
+    x_coordinates_c.append(centroids[i][0])
+    y_coordinates_c.append(centroids[i][1])
+  plt.scatter(x_coordinates_c, y_coordinates_c, color='hotpink')
+  plt.draw()
+
+  plt.show()
+def kmeans(points, k, draw_iteration=False):
+    centroids = init_center(points, k)
+    clusters = calculate_nearest_centroid(points, centroids)
+    privious_centroids = copy.deepcopy(centroids)
+
+    if (draw_iteration):
+        draw(clusters, centroids)
+
+    while True:
+        centroids = centroids_update(centroids, clusters)
+        clusters = calculate_nearest_centroid(points, centroids)
+
+        if (draw_iteration):
+            draw(clusters, centroids)
+
+        if is_equals(centroids, privious_centroids):
+            return {"centroids": centroids, "clusters": clusters}
+
+        privious_centroids = copy.deepcopy(centroids)
+def cluster_count(inertia):
+  min_inertia_ratio = 100000
+  index = 0
+
+  for i in range(len(inertia)):
+    if i == 0 or i == len(inertia) - 1:
+      continue
+    inertia_ratio = (inertia[i] - inertia[i+1])/(inertia[i-1] - inertia[i])
+    if abs(inertia_ratio) < min_inertia_ratio:
+      min_inertia_ratio = inertia_ratio
+      index = i
+  return index + 1
 
 if __name__ == '__main__':
-    dataset = read_csv("train_titanic.csv")
-    first_task(dataset)
-    second_task(dataset)
-    third_task(dataset)
+  n = 100
+  points = random_points(n)
+  inertia = []
+  K = range(1, 8)
+  for i in K:
+      km = kmeans(points, i)
+      clasters = km["clusters"]
+      centroids = km["centroids"]
+      sum = 0
+      for kl in range(len(clasters)):
+          for c in range(len(clasters[kl])):
+              sum += dist(centroids[kl], clasters[kl][c]) ** 2
 
+      inertia.append(sum)
+  plt.plot(K, inertia)
+  k = cluster_count(inertia)
